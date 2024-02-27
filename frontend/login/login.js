60b9139e4d060f44da8ab57a4fba1cf9500580fb
lalitarchan03 <lalitarchanprakashpandey@gmail.com>
2024-02-27 17:23:01 +0530

    Creating Login Screen Frontend

---
+++ frontend/login/login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('login-form');
+
+form.addEventListener('submit', userLogin);
+
+function userLogin(e) {
+    e.preventDefault();
+    const email= e.target.email.value;
+    const password = e.target.password.value;
+    const userDetail = {
+        email,
+        password
+    }
+    // console.log(userDetail);
+
+    axios.post("http://localhost:3000/user/login", userDetail)
+        .then(res => {
+            form.reset();
+        })
+        .catch(err => {
+            form.reset();
+            console.log(err)
+        });
+}
